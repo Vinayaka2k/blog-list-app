3a2f6d7209a41f4aabba485598803b3bb03b1e37
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-02-08 20:30:07 +0530

    added a new schema and model - closed #1

---
+++ models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    author: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = new mongoose.Model('Blog', blogSchema)
