80db1ed042bcd70440ababe5bb4ebc7b6726c05d
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-02-09 10:45:33 +0530

    Added totalLikes in utils/listhelper and defined a testcase - closed #11

--- tests/helper_functions.test.js
+++ tests/helper_functions.test.js
@@ -1,6 +1,14 @@
 const listHelper = require('../utils/list_helper')
+const blogs = require('../utils/blog_list')
 
-test('dummy function returns 1', () => {
-    const blogs = []
-    expect(listHelper.dummy(blogs)).toBe(1)
+describe('Inital Test', () => {
+    test('dummy function returns 1', () => {
+        expect(listHelper.dummy(blogs)).toBe(1)
+    })
+})
+
+describe('total likes', () => {
+    test('when list has only one blog, equals the likes of that', () => {
+      expect(listHelper.totalLikes([blogs[0]])).toBe(7)
+    })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
     return 1
   }
-  
-  module.exports = {
-    dummy
-  }
+
+const totalLikes = (blogs) => {
+    const res = blogs.reduce((accumulator, blog) => accumulator+blog.likes, 0)
+    console.log(res)
+    return res
+}
+
+module.exports = {
+  dummy,
+  totalLikes
+}
