5f7002be46f302d4ef53a0bc3a0c1143ed059047
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-02-09 10:51:24 +0530

    Added few more tests

--- tests/helper_functions.test.js
+++ tests/helper_functions.test.js
@@ -11,4 +11,10 @@ describe('total likes', () => {
     test('when list has only one blog, equals the likes of that', () => {
       expect(listHelper.totalLikes([blogs[0]])).toBe(7)
     })
+    test('when list has multiple blogs, equals sum of likes of all blogs', () => {
+        expect(listHelper.totalLikes(blogs)).toBe(36)
+      })
+    test('when list is empty, equals zero', () => {
+        expect(listHelper.totalLikes([])).toBe(0)
+      })
 })
