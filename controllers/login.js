403bdbcde706e8127828d498122b33af5f6a3211
Vinayaka2k <vinuvinayaka2000@gmail.com>
2022-02-10 09:04:21 +0530

    closed #23 - implemented logging in feature using jwt

---
+++ controllers/login.js
@@ -0,0 +1,22 @@
+const User = require('../models/user')
+const loginRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+loginRouter.post('/', async (request, response) => {
+    const body = request.body
+    const user = await User.findOne({username: body.username})
+    const passwordCorrect = user === null ? false : await bcrypt.compare(body.password, user.passwordHash)
+    if(!passwordCorrect)
+        return response.status(401).json({error: 'invalid username or password'})
+    const userToken = {
+        username: user.username,
+        id: user._id
+    }    
+    const token = jwt.sign(userToken, process.env.SECRET)
+    response.status(200).send({token, username: user.username, name: user.name})
+})
+
+module.exports = loginRouter
+
+
