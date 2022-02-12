5cb71d0d67c32c76a19650e7ff7e08e93a0a8a1c
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 12:10:25 -0600

    Added semi-colons to ends of lines.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,56 +4,56 @@ const User = require('../models/user')
 const endpointHandler = require(`../handlers/endpointHandler`);
 
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-    response.json(blogs)
-})
+    const blogs = await Blog.find({});
+    response.json(blogs);
+});
 
 blogRouter.get('/:id', async (request, response) => {
-    const blog = Blog.findById(request.params.id)
+    const blog = Blog.findById(request.params.id);
     if (blog)
-        response.json(blog)
+        response.json(blog);
     else
-        response.status(404).end()
-})
+        response.status(404).end();
+});
 
 blogRouter.post('/', async (request, response, next) => {
     endpointHandler.runWithValidation(request, response, token => {
-        const user = await User.findById(token)
+        const user = await User.findById(token);
 
-        const body = request.body
+        const body = request.body;
         const blog = new Blog({
             title: body.title,
             url: body.url,
             author: body.author,
             likes: body.likes,
             user: user._id
-        })
-        const newBlog = await blog.save()
-        user.blogs = user.blogs.concat(newBlog._id)
-        await user.save()
-        return response.status(201).json(newBlog)
+        });
+        const newBlog = await blog.save();
+        user.blogs = user.blogs.concat(newBlog._id);
+        await user.save();
+        return response.status(201).json(newBlog);
     });
-})
+});
 
 blogRouter.delete('/:id', async (request, response) => {
     endpointHandler.runWithValidation(request, response, token => {
-        await Blog.findByIdAndRemove(request.params.id)
-        return response.status(204).end()
+        await Blog.findByIdAndRemove(request.params.id);
+        return response.status(204).end();
     });
-})
+});
 
 blogRouter.put('/:id', async (request, response) => {
     endpointHandler.runWithValidation(request, response, token => {
-        const body = request.body
+        const body = request.body;
         const blog = {
             title: body.title,
             url: body.url,
             author: body.author,
             likes: body.likes
-        }
-        const updatedBlog = Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-        return response.json(updatedBlog)
+        };
+        const updatedBlog = Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
+        return response.json(updatedBlog);
     });
-})
+});
 
-module.exports = blogRouter
+module.exports = blogRouter;
