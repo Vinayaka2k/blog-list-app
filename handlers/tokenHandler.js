9a47c2704775b80f17ce0f8280b11f5e0cd620cd
tacosontitan <jkdavis1691@gmail.com>
2022-02-12 11:57:59 -0600

    Simplified token handler.

--- handlers/tokenHandler.js
+++ handlers/tokenHandler.js
@@ -13,43 +13,20 @@ function getBearerTokenFromRequest(request) {
 
     return token;
 }
-/**
- * Generates a validation response object.
- * @param {Boolean} isValid Is the token considered valid?
- * @param {Integer} status What status code should the consumer provide their response object?
- * @param {String} message What message should the consumer provide their response object?
- * @returns Returns a validation response object containing the validation result and appropriate response data.
- */
-function generateValidationResult(valid, status, message) {
-    return {
-        valid: valid,
-        response: {
-            status: status,
-            message: message
-        }
-    }
-}
 module.exports = {
     /**
-     * Validates the authentication token for a specified request object.
-     * @param {apiRequest} request The request requiring token validation.
-     * @returns Returns a validation response object containing the validation result and appropriat4e response data.
+     * Run a specified action with token validation.
+     * @param {ApiRequest} request The request object being utilized in the endpoint.
+     * @param {ApiResponse} response The response object being utilized in the endpoint.
+     * @param {Callback} action The callback to be invoked when a valid token is found.
+     * @returns Returns the response object from the specified callback if the token is valid, otherwise the provided response object with a status code of 401 and an invalid token error message.
      */
-    validateToken: function(request) {
+    runWithValidation: function(request, response, action) {
         let token = getBearerTokenFromRequest(request);
         let decodedToken = jwt.verify(token, process.env.SECRET)
         if (decodedToken.id)
-            return generateValidationResult(true, 200, 'The provided token is valid.');
-        else
-            return generateValidationResult(false, 401, 'Invalid token.')
-    },
-    /**
-     * Populates a response object with the specified validation result.
-     * @param {ApiResponse} response The response object that should be sent back to the consumer.
-     * @param {TokenValidationResult} validationResult The result of token validation.
-     */
-    populateReponse: function(response, validationResult) {
-        return response.status(validationResult.response.status)
-                       .json({ error: validationResult.response.message });
+            return action(decodedToken.id);
+        
+        return response.status(401).json({ error: 'Invalid token.' });
     }
 }
